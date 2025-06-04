12a88559d4ce4663627c6dec2c7840980e71c4e4
v1dgt <v1dgt@mail.ru>
2025-06-04 16:32:29 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default (description, generateRound) => {
   console.log(description)
   for (let i = 0; i < ROUNDCOUNT; i += 1) {
     const { question, answer } = generateRound()
-    console.log(`Question: ${String(question).trim()}`)
+    console.log(`Question: ${question}`)
     const userAnswer = readlineSync.question('Your answer: ')
     if (userAnswer === answer) {
       console.log('Correct!')
